f739fab313ae8a4a9e865f2b07b2036ed74ea1b8
USER <USER@DESKTOP-6K31JSS>
2022-03-24 20:21:23 -0700

    first commit

---
+++ SpringApplicationContext/src/org/com/ApplicationContextMain.java
@@ -0,0 +1,11 @@
+package org.com;
+ import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class ApplicationContextMain{
+ public static void main(String[] args) {
+ ApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+Country countryObj = (Country) appContext.getBean("country");
+System.out.println("India's Capital Name:"+countryObj.getCapitalName("capital"));
+}
+}
